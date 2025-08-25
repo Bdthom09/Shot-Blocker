9c8b370a067dc8c9883c6e23915a98a667b193ae
Bdthom09 <45083940+Bdthom09@users.noreply.github.com>
2025-08-25 16:31:58 -0500

    Initial Commit

---
+++ Shot Blocker/Shot_BlockerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shot_BlockerApp.swift
+//  Shot Blocker
+//
+//  Created by Bryant D. Thomas on 8/25/25.
+//
+
+import SwiftUI
+
+@main
+struct Shot_BlockerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shot BlockerTests/Shot_BlockerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Shot_BlockerTests.swift
+//  Shot BlockerTests
+//
+//  Created by Bryant D. Thomas on 8/25/25.
+//
+
+import Testing
+@testable import Shot_Blocker
+
+struct Shot_BlockerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
